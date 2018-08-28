423794739ec99a6e512492f59d80b21ddb0d749c
iron <994334509@qq.com>
2018-08-28 11:03:29 +0800

    表名查询工具及测试

--- src/main/java/com/hitbd/proj/util/Utils.java
+++ src/main/java/com/hitbd/proj/util/Utils.java
@@ -1,5 +1,8 @@
 package com.hitbd.proj.util;
 
+import com.hitbd.proj.Settings;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils {
@@ -9,7 +12,11 @@ public class Utils {
      * @param date 该Alarm的创建时间
      * @return 对应的表名
      */
-    String getTableName(Date date) {
-        return null;
+    public static String getTableName(Date date) {
+        long milliSecond = date.getTime() - Settings.BASETIME;
+        int period = (int)((milliSecond + 1) / (1000 * 60 * 60 * 24 * 4));
+        Date result = new Date(Settings.BASETIME + period * 1000 * 60 * 60 * 24 * 4);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMdd");
+        return "alarm_" + simpleDateFormat.format(result);
     }
 }
