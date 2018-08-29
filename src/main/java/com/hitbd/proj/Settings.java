679021dd971e696a232fd4e43c347bf1e88072c3
iron <994334509@qq.com>
2018-08-29 12:10:33 +0800

    添加上传文件夹功能

--- src/main/java/com/hitbd/proj/Settings.java
+++ src/main/java/com/hitbd/proj/Settings.java
@@ -3,4 +3,6 @@ package com.hitbd.proj;
 public class Settings {
     public static long BASETIME = 1262275200000L; //2010年1月1日 00:00:00
     public static String QUERY_LOG_FILENAME = "Query%d.txt"; //查询结果文件名
+    public static String igniteHostAddress = "127.0.0.1";
+    public static String logDir = "./";
 }
