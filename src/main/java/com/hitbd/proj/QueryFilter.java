8d2d1efbce4d1d6999b73ef9b3c368931ca6459d
IronWei <994334509@qq.com>
2018-09-20 10:29:09 +0800

    增加过滤器的功能

--- src/main/java/com/hitbd/proj/QueryFilter.java
+++ src/main/java/com/hitbd/proj/QueryFilter.java
@@ -7,23 +7,33 @@ import java.util.List;
 import java.util.Set;
 
 public class QueryFilter {
+    private Boolean allowReadStatus;
+    private Set<String> allowAlarmStatus;
+    private Set<String> allowAlarmType;
+    private Pair<Date, Date> allowTimeRange;
 
     public Set<String> getAllowAlarmType() {
         return allowAlarmType;
     }
-
     public void setAllowAlarmType(Set<String> allowAlarmType) {
         this.allowAlarmType = allowAlarmType;
     }
-
     public Pair<Date, Date> getAllowTimeRange() {
         return allowTimeRange;
     }
-
     public void setAllowTimeRange(Pair<Date, Date> allowTimeRange) {
         this.allowTimeRange = allowTimeRange;
     }
-
-    private Set<String> allowAlarmType;
-    private Pair<Date, Date> allowTimeRange;
+    public Set<String> getAllowAlarmStatus() {
+        return allowAlarmStatus;
+    }
+    public void setAllowAlarmStatus(Set<String> allowAlarmStatus) {
+        this.allowAlarmStatus = allowAlarmStatus;
+    }
+    public Boolean getAllowReadStatus() {
+        return allowReadStatus;
+    }
+    public void setAllowReadStatus(Boolean allowReadStatus) {
+        this.allowReadStatus = allowReadStatus;
+    }
 }
